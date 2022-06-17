ef9067d466d6b414db2871f10bf15ec580a36dd5
coder519 <95204792+Coder519@users.noreply.github.com>
2022-06-17 20:32:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ to_number = Number (content);
+
+ if(Number.isInteger(to_number));
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+  
+  
+  
+  }
+
+function setup() {
+    screen_width = window.innerWidth;
+    canvas = createCanvas(900,600);
+  
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+    document.getElementById("status").innerHTML = to_number+"Apples drawn";
+    for (var i =1; i <= to_number; i++){
+      x =Math.floor(Math.random)*700;
+      y =Math.floor(Math.random)*400;
+      image(apple,x,y,50,50);
+    }
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
